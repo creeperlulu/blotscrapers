dfc60de91cd25a31d61605403ad48dd20489471a
Creeperlulu <lucasboucher2006@gmail.com>
2024-08-25 01:11:19 +0200

    Added randomizer and lines to buildings (unfinished)

--- project.js
+++ project.js
@@ -5,41 +5,41 @@ const t = new bt.Turtle()
 const u = new bt.Turtle()
 
 const numSkyScrapers = 24;
-const SecLayerNumSkyScrapers = 24;
-const skyScraperHeight = bt.randInRange;
+const SecLayerNumSkyScrapers = 12;
+const skyScraperRandomizer = bt.randInRange;
+const fullWidth = 0;
 
 const skyScrapers = [];
 const scraperGen = [];
 
-const bg = [
-    [0,0],
-    [0,height],
-    [height, width],
-    [height, 0],
-    [0,0]
-];
-
 for (let i = 0; i < numSkyScrapers; i++) {
   t.setAngle(90);
-  const tempHeight = skyScraperHeight(10,50)
+  const tempHeight = skyScraperRandomizer(10,50)
   t.forward(tempHeight);
   t.setAngle(0);
-  t.forward(width / numSkyScrapers);
+  const tempDivider = skyScraperRandomizer(numSkyScrapers - 18, numSkyScrapers +25)
+  t.forward(width / tempDivider);
   t.setAngle(-90);
   t.forward(tempHeight);
+  fullWidth = fullWidth + (width / tempDivider)
 }
 
-for (let i = 0; i < numSkyScrapers; i++) {
+for (let i = 0; i < SecLayerNumSkyScrapers; i++) {
   u.setAngle(90);
-  const tempHeight = skyScraperHeight(10,50)
+  
+  const tempHeight = skyScraperRandomizer(10,50)
   u.forward(tempHeight);
   u.setAngle(0);
-  u.forward(width / numSkyScrapers);
+  u.forward(width / SecLayerNumSkyScrapers);
   u.setAngle(-90);
   u.forward(tempHeight);
 }
 
-drawLines([bg], {fill:"#000"});
+const BackScrapersLines = bt.offset(u.lines(), 0.2);
+
+drawLines(u.lines(), {fill:"#525252"});
+drawLines(BackScrapersLines, {fill:"#000"});
 drawLines(t.lines(), {fill:"#FFF"});
+drawLines(t.lines());
 
 // Made with ❤ by Creeperlulu
