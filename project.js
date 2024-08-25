ee8596062d3cc915a760c5d6ac86a591ffff29a8
Creeperlulu <lucasboucher2006@gmail.com>
2024-08-25 16:47:09 +0200

    Fixed skyscrapers generation to make them fit perfectly inside the frame + added function for better readability

--- project.js
+++ project.js
@@ -1,45 +1,46 @@
 const width = 125;
 const height = 125;
 setDocDimensions(width, height);
-const t = new bt.Turtle()
-const u = new bt.Turtle()
 
 const numSkyScrapers = 24;
 const SecLayerNumSkyScrapers = 12;
 const skyScraperRandomizer = bt.randInRange;
-const fullWidth = 0;
+var fullWidth;
 
 const skyScrapers = [];
 const scraperGen = [];
 
-for (let i = 0; i < numSkyScrapers; i++) {
-  t.setAngle(90);
+function createSkyScrapers() {
+  const turtle = new bt.Turtle()
+  fullWidth = 0;
+while (fullWidth < width) {
+  turtle.setAngle(90);
   const tempHeight = skyScraperRandomizer(10,50)
-  t.forward(tempHeight);
-  t.setAngle(0);
+  turtle.forward(tempHeight);
+  turtle.setAngle(0);
   const tempDivider = skyScraperRandomizer(numSkyScrapers - 18, numSkyScrapers +25)
-  t.forward(width / tempDivider);
-  t.setAngle(-90);
-  t.forward(tempHeight);
+  if (fullWidth + (width / tempDivider) >= width) {
+    turtle.forward(width - fullWidth);
+    turtle.setAngle(-90);
+    turtle.forward(tempHeight);
+    break;
+      } else {
+    turtle.forward(width / tempDivider);
+    turtle.setAngle(-90);
+  turtle.forward(tempHeight);
+  }
+  
   fullWidth = fullWidth + (width / tempDivider)
 }
-
-for (let i = 0; i < SecLayerNumSkyScrapers; i++) {
-  u.setAngle(90);
-  
-  const tempHeight = skyScraperRandomizer(10,50)
-  u.forward(tempHeight);
-  u.setAngle(0);
-  u.forward(width / SecLayerNumSkyScrapers);
-  u.setAngle(-90);
-  u.forward(tempHeight);
+  return turtle.lines();
 }
 
-const BackScrapersLines = bt.offset(u.lines(), 0.2);
 
-drawLines(u.lines(), {fill:"#525252"});
-drawLines(BackScrapersLines, {fill:"#000"});
-drawLines(t.lines(), {fill:"#FFF"});
-drawLines(t.lines());
+const u = createSkyScrapers()
+const t = createSkyScrapers()
+drawLines(u, {fill:"#FFF"});
+drawLines(u);
+drawLines(t, {fill:"#FFF"});
+drawLines(t);
 
 // Made with ❤ by Creeperlulu
