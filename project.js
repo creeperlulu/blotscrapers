93017a1423697fc38d6a38287cdb4e168fedde02
Creeperlulu <lucasboucher2006@gmail.com>
2024-08-25 18:14:17 +0200

    Improved readability and started working on star shape

--- project.js
+++ project.js
@@ -2,14 +2,17 @@ const width = 125;
 const height = 125;
 setDocDimensions(width, height);
 
-const numSkyScrapers = 24;
-const SecLayerNumSkyScrapers = 12;
+// Edit these values to change the look of the artwork!
+const skyScrapersIntensity = 12;
+
+
+
 const skyScraperRandomizer = bt.randInRange;
 var fullWidth;
-
 const skyScrapers = [];
 const scraperGen = [];
 
+// Generate skyscrapers
 function createSkyScrapers() {
   const turtle = new bt.Turtle()
   fullWidth = 0;
@@ -18,7 +21,7 @@ while (fullWidth < width) {
   const tempHeight = skyScraperRandomizer(10,50)
   turtle.forward(tempHeight);
   turtle.setAngle(0);
-  const tempDivider = skyScraperRandomizer(numSkyScrapers - 18, numSkyScrapers +25)
+  const tempDivider = skyScraperRandomizer(skyScrapersIntensity, skyScrapersIntensity +25)
   if (fullWidth + (width / tempDivider) >= width) {
     turtle.forward(width - fullWidth);
     turtle.setAngle(-90);
@@ -29,18 +32,30 @@ while (fullWidth < width) {
     turtle.setAngle(-90);
   turtle.forward(tempHeight);
   }
-  
   fullWidth = fullWidth + (width / tempDivider)
 }
   return turtle.lines();
 }
 
+function createStars(nbr){
+  for (let i = 0; i < nbr; i++) {
+    
+  }
+}
 
+const star = bt.nurbs([[0, 0], [1, 0], [1, 1]], { steps: 4, degree: 2 });
+const star2 = bt.nurbs([[0, 0], [0, -1], [1, -1]], { steps: 4, degree: 2 });
+const star3 = bt.nurbs([[0, 0], [-1, 0], [-1, -1]], { steps: 4, degree: 2 });
+const star4 = bt.nurbs([[0, 0], [1, 0], [1, 1]], { steps: 4, degree: 2 });
+
+// Print the skyscrapers lines
 const u = createSkyScrapers()
 const t = createSkyScrapers()
+//const stars = createStars(nbrStars)
 drawLines(u, {fill:"#FFF"});
 drawLines(u);
 drawLines(t, {fill:"#FFF"});
 drawLines(t);
+drawLines([star]);
 
 // Made with ❤ by Creeperlulu
